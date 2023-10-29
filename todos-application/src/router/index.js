59efe9543eb207bf16b67d25a473b4649322f107
Do Truong Manh <manhcntt2.1@gmail.com>
2023-10-29 23:46:34 +0700

    [todos application] add router

--- todos-application/src/router/index.js
+++ todos-application/src/router/index.js
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'HomePage',
+      component: () => import('../views/HomePage.vue')
+    },
+    {
+      path: '/archive',
+      name: 'ArchivePage',
+      component: () => import('../views/ArchivePage.vue')
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      name: 'About',
+      component: () => import('../views/AboutPage.vue')
     }
   ]
 })
