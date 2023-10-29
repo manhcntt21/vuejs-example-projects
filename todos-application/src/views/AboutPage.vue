aef90569d2d557d8999c07129f6565bb4d517fb3
Do Truong Manh <manhcntt2.1@gmail.com>
2023-10-29 23:46:04 +0700

    [todos application] init views: about, archive, home

---
+++ todos-application/src/views/AboutPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <slot></slot>
+</template>
+
+<script>
+export default {
+  name: "AboutPage"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ todos-application/src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <slot></slot>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<style scoped>
+
+</style>
