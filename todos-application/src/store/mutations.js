f3f09f5d085fff2b967ba7cf2a20323fa8357b12
Do Truong Manh <manhcntt2.1@gmail.com>
2023-10-30 09:27:26 +0700

    [todos application] add mutations for store

--- todos-application/src/store/mutations.js
+++ todos-application/src/store/mutations.js
@@ -1,1 +1,47 @@
-export default {};
+export default {
+    updateTodo(state, todoItem) {
+        let {id, completed, name} = todoItem
+        let findEl = state.todos.find(x => x.id === id)
+        if(findEl) {
+            if(completed !== undefined) {
+                findEl.completed = completed
+            }
+
+            if(name !== undefined) {
+                findEl.name = name
+            }
+        } else {
+            console.log(`To Do List Item ${id} couldn't be found`)
+        }
+    },
+
+    addTodo(state, todoItem) {
+        if(todoItem.id !== undefined && typeof  todoItem.name == 'string' && typeof todoItem.completed == 'boolean') {
+            state.todos.push({
+                id: todoItem.id,
+                name: todoItem.name,
+                completed: todoItem.completed,
+                location: 'home'
+            })
+        }
+    },
+
+    deleteTodo(state, todoItem) {
+        let id = todoItem.id
+        let removedEl = state.todos.findIndex(x => x.id == id)
+        if(removedEl) {
+            state.todos.splice(removedEl, 1)
+        }
+    },
+
+    moveTodoItem(state, todoItem) {
+        let id = todoItem.id
+        let location =  todoItem.location
+        let findEl = state.todos.findIndex(x => x.id == id)
+        if(findEl) {
+            findEl.location = location
+        } else {
+            console.log(`Todo List Item ${id} couldn't be found'`)
+        }
+    }
+};
